f976c7b9b5cdad587d5dc85678419c98dec5e31a
Chen Xu <cxu369@gatech.edu>
2023-02-03 18:41:40 -0600

    Update Lists1Exercises.java

--- exercises/lists1/Lists1Exercises.java
+++ exercises/lists1/Lists1Exercises.java
@@ -4,15 +4,27 @@ public class Lists1Exercises {
       * to change. */
     public static IntList incrList(IntList L, int x) {
         /* Your code here. */
-        return L;        
+        IntList Q;
+        if (L.rest == null){
+             Q = new IntList(L.first + x,null);
+            return Q;
+        } else{
+            Q = new IntList(L.first + x, incrList(L.rest, x));
+        }
+        return Q;
     }
 
     /** Returns an IntList identical to L, but with
       * each element incremented by x. Not allowed to use
       * the 'new' keyword. */
     public static IntList dincrList(IntList L, int x) {
-        /* Your code here. */
-        return L;
+        IntList Q = L;
+        Q.first += x;
+        if (Q.rest == null){
+            return Q;
+        }else{
+            return dincrList(L.rest, x);
+        }
     }
 
     public static void main(String[] args) {
@@ -26,8 +38,8 @@ public class Lists1Exercises {
         // Test your answers by uncommenting. Or copy and paste the
         // code for incrList and dincrList into IntList.java and
         // run it in the visualizer.
-        // System.out.println(L.get(1));
-        // System.out.println(incrList(L, 3));
-        // System.out.println(dincrList(L, 3));        
+        System.out.println(L.get(1));
+        System.out.println(incrList(L, 3));
+        System.out.println(dincrList(L, 3));
     }
 }
