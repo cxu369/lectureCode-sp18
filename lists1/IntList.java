f2fa6cc87fde6bd6a4062206feb0142f2164af36
Chen Xu <cxu369@gatech.edu>
2023-02-03 18:36:52 -0600

    Finish list practice

--- lists1/IntList.java
+++ lists1/IntList.java
@@ -1,44 +1,96 @@
-public class IntList {
-	public int first;
-	public IntList rest;
+@@ -0,0 +1,95 @@
+/** Array based list.
+ *  @author Josh Hug
+ */
 
-	public IntList(int f, IntList r) {
-		first = f;
-		rest = r;
+/* The next item ALWAYS goes in the size position */
+
+public class AList<Item> implements List61B<Item>{
+	/* the stored integers */
+	private Item[] items;
+	private int size;
+
+	private static int RFACTOR = 2;
+
+	/** Creates an empty list. */
+	public AList() {
+		size = 0;
+		items = (Item[]) new Object[100];
 	}
 
-	/** Return the size of the list using... recursion! */
+	/** Resize our backing array so that it is
+	 * of the given capacity. */
+	private void resize(int capacity) {
+		Item[] a = (Item[]) new Object[capacity];
+		System.arraycopy(items, 0, a, 0, size);
+		items = a;
+	}
+
+	@Override
+	/** Inserts X into the back of the list. */
+	public void addLast(Item x) {
+		if (size == items.length) {
+			resize(size * RFACTOR);
+		}
+
+		items[size] = x;
+		size = size + 1;
+	}
+
+	@Override
+	/** Returns the item from the back of the list. */
+	public Item getLast() {
+		int lastActualItemIndex = size - 1;
+		return items[lastActualItemIndex];
+	}
+
+	@Override
+	/** Gets the ith item in the list (0 is the front). */
+	public Item get(int i) {
+		return items[i];
+	}
+
+	@Override
+	/** Returns the number of items in the list. */
 	public int size() {
-		if (rest == null) {
-			return 1;
-		}
-		return 1 + this.rest.size();
+		return size;
 	}
 
-	/** Return the size of the list using no recursion! */
-	public int iterativeSize() {
-		IntList p = this;
-		int totalSize = 0;
-		while (p != null) {
-			totalSize += 1;
-			p = p.rest;
-		}
-		return totalSize;
+	@Override
+	/** Deletes item from back of the list and
+	 * returns deleted item. */
+	public Item removeLast() {
+		Item itemToReturn = getLast();
+		/* setting to zero not strictly necessary, but
+		 * it's a good habit (we'll see why soon) */
+		items[size - 1] = null;
+		size = size - 1;
+		return itemToReturn;
 	}
 
-	/** Returns the ith item of this IntList. */
-	public int get(int i) {
-		if (i == 0) {
-			return first;
-		}
-		return rest.get(i - 1);
+	@Override
+	/** Inserts item into given position.
+	 * Code from discussion #3 */
+	public void insert(Item x, int position) {
+		Item[] newItems = (Item[]) new Object[items.length + 1];
+
+		System.arraycopy(items, 0, newItems, 0, position);
+		newItems[position] = x;
+
+		System.arraycopy(items, position, newItems, position + 1, items.length - position);
+		items = newItems;
 	}
 
-	public static void main(String[] args) {
-		IntList L = new IntList(15, null);
-		L = new IntList(10, L);
-		L = new IntList(5, L);
-
-		System.out.println(L.get(100));
+	@Override
+	/** Inserts an item at the front. */
+	public void addFirst(Item x) {
+		insert(x, 0);
 	}
-} 
+
+	@Override
+	/** Gets an item from the front. */
+	public Item getFirst() {
+		return get(0);
+	}
+
+}
